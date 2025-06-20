1a68591cf1ce6234eca13be439fc97a884d5d6a0
Asante kelvin <asantekelvin229@gmail.com>
2025-06-20 03:02:44 -1200

    Trigger new deployment after Vercel config fix

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello, CI/CD! (Attempt 2 - Simpler)');
+  res.send('Hello, CI/CD! (Attempt 2 - Simpler  Asante kelvin)');
+
 });
 
 app.listen(port, () => {
